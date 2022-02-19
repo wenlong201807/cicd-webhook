a891d34e1916a4499237eb6ad0a29e2d46be8456
dragon <1573511441@qq.com>
2022-02-19 23:30:25 +0800

    bug:log 方式查找中1

--- webhook.js
+++ webhook.js
@@ -5,7 +5,9 @@ const sendMail = require("./sendMail");
 const SECRET = "zwl@157351";
 
 function sign(body) {
-  return `sha1=` + crypto.createHash("sha1", SECRET).update(body).digest("hex");
+  const r = `sha1=` + crypto.createHash("sha1", SECRET).update(body).digest("hex");
+  console.log('webhook-sign ->: ', r);
+  return r;
 }
 let server = http.createServer((req, res) => {
   console.log("触发钩子自动更新执行:", req.method, req.url, req.headers);
@@ -25,14 +27,17 @@ let server = http.createServer((req, res) => {
       // github请求来的时候，要传递请求体，
       // 另外还会穿一个signature过来，需要验证签名对不对
       let signature = req.headers["x-hub-signature"];
+      // 以上正常，问题在这之下--------------------------------------------
       if (signature !== sign(body)) {
+        console.log('两处的密码不同!')
         return res.end("not Allowed");
       }
 
       // 添加部署脚本
-      if(event === 'push') {
+      if(event == 'push') {
         console.log('部署脚本自动执行')
         let payload = JSON.parse(body);
+        console.log('部署脚本自动执行payload: ', payload);
         // $ sh vue-cicd-front.sh
         // $ sh cicd-back.sh
         let child = spawn('sh', [`./${payload.repository.name}.sh`]);
