63f88e6fbf0a461da54944013be14ce06f6be874
wenlongmac <2854588836@qq.com>
2022-02-19 20:07:51 +0800

    feat:添加后台服务执行脚本

---
+++ vue-back.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORK_PATH='/opt/cicd-pro/cicd-back'
+cd $WORK_PATH
+echo '先清除老代码'
+git reset --hard origin/main
+git clean -f 
+echo '拉取最新代码'
+git pull origin main
+echo '开始执行构建'
+docker build -t vue-back .
+echo '停止旧容器并删除旧容器'
+docker stop vue-back-container
+docker rm vue-back-container
+echo '启动新容器'
+docker container run -p 6006:6006 --name vue-back-container -d vue-back
--- webhook.js
+++ webhook.js
@@ -1,7 +1,28 @@
 let http = require("http");
+let crypto = require("crypto");
+const SECRET = "zwl@157351";
+
+function sign(body) {
+  return `sha1=` + crypto.createHash("sha1", SECRET).update(body).digest("hex");
+}
 let server = http.createServer((req, res) => {
-  console.log('触发钩子自动更新执行:', req.method, req.url);
+  console.log("触发钩子自动更新执行:", req.method, req.url);
   if (req.method === "POST" && req.url === "/webhook") {
+    let buffers = [];
+    req.on("data", (buffer) => {
+      buffers.push(buffer);
+    });
+
+    req.on("end", (buffer) => {
+      let body = Buffer.concat(buffers);
+      let event = req.headers["x-github-event"]; // event=push 事件
+      // github请求来的时候，要传递请求体，
+      // 另外还会穿一个signature过来，需要验证签名对不对
+      let signature = req.headers["x-hub-signature"];
+      if (signature !== sign(body)) {
+        return res.end("not Allowed");
+      }
+    });
     // res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader("Content-Type", "application/json;charset=utf-8");
     res.end(JSON.stringify({ ok: true }));
